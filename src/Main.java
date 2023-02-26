906d0bfc711b00cd0aa651025f740f07ec70690f
GaripovDaniil <Garipov.Daniil@urfu.me>
2023-02-26 12:22:37 +0500

    Таблица углов и синусов

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите Ваше имя: ");
+        String name = scanner.nextLine();
+        System.out.println("Приветствую, " + name + "\n\n");
+        System.out.println("Угол\tСинус\n");
+        for(int angle = 0; angle <= 45; angle += 5){
+            var sine = Math.sin(Math.toRadians(angle));
+            System.out.println(angle + "\t\t" + sine);
+        }
     }
 }
