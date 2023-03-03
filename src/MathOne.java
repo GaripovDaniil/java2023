7c687261ab52895552c6c65bffffe2310dacf266
GaripovDaniil <Garipov.Daniil@urfu.me>
2023-03-03 17:36:32 +0500

    Комментарии к fact и sign

--- src/MathOne.java
+++ src/MathOne.java
@@ -1,6 +1,11 @@
 package src;
 
 public class MathOne {
+    /**
+     * <p>Находит факториал введенного числа</p>
+     * @param number Число, факториал которого нужно найти
+     * @return number!; 1, если введен 0 или 1; IllegalArgumentException, если введено отрицательное число
+     */
     public static int fact(int number){
         if (number < 0) throw new IllegalArgumentException();
         if (number == 0 || number == 1){
@@ -13,6 +18,11 @@ public class MathOne {
         return result;
     }
 
+    /**
+     * <p>Находит сигнум введенного числа</p>
+     * @param number число, знак которого нужно извлечь
+     * @return 0, если введен 0; 1, если введено положительное число; -1, если введено отрицательное число
+     */
     public static int sign(int number){
         return Integer.compare(number, 0);
     }
